71307113a20d918ac5972262a94191e9de75448a
Amal Wickramarathna <wickramarathna.amal@gmail.com>
2023-12-29 00:02:33 +0530

    initial commit

--- flutter_application_1/lib/features/counter_event.dart
+++
@@ -1,5 +0,0 @@
-class CounterEvent {}
-
-class NumberIncreaseEvent extends CounterEvent {}
-
-class NumberDecreaseEvent extends CounterEvent {}
--- flutter_application_1/lib/features/counter_state.dart
+++
@@ -1,8 +0,0 @@
-class CounterState {}
-
-class InitialState extends CounterState {}
-
-class UpdateState extends CounterState {
-  final int counter;
-  UpdateState(this.counter);
-}
